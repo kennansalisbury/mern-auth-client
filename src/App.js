9a8ca2ad528fa2ec64a2622ed6634dfd525175ca
kennansalisbury <kennansalisbury@gmail.com>
2020-01-23 16:36:19 -0800

    set up app, add user functions in app state

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // Import packages
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router } from 'react-router-dom'
 
 // Resources and custom components
@@ -13,13 +13,42 @@ const App = props => {
   // Declare state variables
   let [user, setUser] = useState(null)
 
+  // Define any onload actions (i.e. to look out for the token)
+  useEffect(() => {
+    console.log('check for token')
+    decodeToken()
+  }, [])
+
+  // Helper function to update the user
+  const updateUser = newToken => {
+    if(newToken) {
+      // Save the token 
+      localStorage.setItem('userToken', newToken) //reassigns token in local storage
+
+      // update the user with token info
+      decodeToken(newToken)
+    }
+    else { // logging out
+      setUser(null)
+    }
+  }
+
+  // Helper function to decode existing tokens
+  const decodeToken = existingToken => {
+    let token = existingToken || localStorage.getItem('userToken')
+    console.log('The token is:', token)
+
+    //decode token
+
+  }
+
   return (
     <Router>
       <div className="App">
-        <Nav />
+        <Nav user={user} updateUser={updateUser} />
         <Header />
         <main>
-          <Content />
+          <Content user={user} updateUser={updateUser} />
         </main>
         <Footer />
       </div>
